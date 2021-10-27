2c4fb7f3659ab34f9394a3ddd5e30b6389108f54
carward25 <83989145+carward25@users.noreply.github.com>
2021-10-26 19:20:43 -0700

    Update Taxicab.py

--- Taxicab.py
+++ Taxicab.py
@@ -3,43 +3,24 @@
 #description: program that holds three private data members for x, y coordinates and current odometer reading
 
 class Taxicab:
+    def __init__(self, x, y):
+        self.x_coord = x
+        self.y_coord = y
+        self.odometer = 0
 
+    def move_x(self, value):
+        self.x_coord += value
+        self.odometer += abs(value)
 
-def __init__(self, x, y):
-    self.__x = x
+    def move_y(self, value):
+        self.y_coord += value
+        self.odometer += abs(value)
 
+    def get_x_coord(self):
+        return self.x_coord
 
-self.__y = y
-self.__odometer = 0
+    def get_y_coord(self):
+        return self.y_coord
 
-
-def get_x(self):
-    return self.__x
-
-
-def get_y(self):
-    return self.__y
-
-
-def get_odometer(self):
-    return self.__odometer
-
-
-def move_x(self, x):
-    self.__x = self.__x + x
-
-
-self.__odometer = self.__odometer + abs(x)
-
-
-def move_y(self, y):
-    self.__y = self.__y + y
-
-
-self.__odometer = self.__odometer + abs(y)
-
-cab = Taxicab(5, -8)
-cab.move_x(3)
-cab.move_y(-4)
-cab.move_x(-1)
-print(cab.get_odometer())
+    def get_odometer(self):
+        return self.odometer
